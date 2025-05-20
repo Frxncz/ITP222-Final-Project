a8620b4b9263c69cc649d9fd66449e78c04783bc
Syk <150402585+sikyel@users.noreply.github.com>
2025-05-20 22:30:14 +0800

    Update and rename users.sql to auth_system.sql
    personal sql used locally

--- users.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    failed_attempts INT DEFAULT 0,
-    lock_until DATETIME DEFAULT NULL
-);
